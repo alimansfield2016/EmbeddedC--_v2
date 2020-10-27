3e5e449bfe5e5983b5f9b4e90cb51a8ffef86ded
Alex <atm2g19@soton.ac.uk>
2020-10-27 10:39:38 +0000

    not all things need to be volatile.

--- libusb++/src/libusb_ext.hpp
+++ libusb++/src/libusb_ext.hpp
@@ -15,15 +15,15 @@ The <> header includes:
 extern "C" {
 	#include "usbdrv.h"
 
-	extern volatile uint8_t usbInputBufOffset;
-	extern volatile uint8_t usbRxBuf[11];
-	extern volatile uint8_t usbDeviceAddr;
-	extern volatile uint8_t usbCurrentTok;
-	extern volatile uint8_t usbRxLen;
+	extern uint8_t usbInputBufOffset;
+	extern uint8_t usbRxBuf[11];
+	extern uint8_t usbDeviceAddr;
+	extern uint8_t usbCurrentTok;
+	extern uint8_t usbRxLen;
 	// extern uint8_t usbTxLen;
 	// extern uint8_t usbTxBuf[11];
 	// extern uint8_t usbRxToken;
-	extern volatile uint8_t usbNewDeviceAddr;
+	extern uint8_t usbNewDeviceAddr;
 
 	// extern uint8_t* usbTxLens[16];
 	extern volatile uint8_t* usbTxLenBufs[16];
